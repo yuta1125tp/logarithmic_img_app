8a29b22a137452f70956a26e58e2438eafddba96
yshira <yuta1125tp@gmail.com>
2022-01-04 16:31:04 +0900

    change default image url

--- docs/main.js
+++ docs/main.js
@@ -95,7 +95,7 @@ const RootComponent = {
       // radius_small: 30,
       // radius_large: 80,
       image_url:
-        "https://cdn.pixabay.com/photo/2016/08/30/03/16/watercolor-1629721__340.jpg",
+        "https://raw.githubusercontent.com/yuta1125tp/logarithmic_img_app/master/assets/RGB_color_wheel_24.jpg",
     };
   },
   computed: {
